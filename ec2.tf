fdb5af44bbfd165f03cffedaf924e07e35e4f562
titan <--list>
2023-11-01 16:13:08 -0500

    updated

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2-one" {
     vpc_security_group_ids = [ aws_security_group.ec2.id ]
     subnet_id = aws_subnet.private_subnet1.id
     user_data = file("userdata.sh")
-    key_name = ""
+    key_name = "aws_key_pair.ec2_key.key_name"
     instance_type = "t2.micro"
     tags = {
         Name = "webserver1"
@@ -16,7 +16,7 @@ resource "aws_instance" "ec2-two" {
     vpc_security_group_ids = [ aws_security_group.ec2.id ]
     subnet_id = aws_subnet.private_subnet2.id
     user_data = file("userdata.sh")
-    key_name = ""
+    key_name = "aws_key_pair.ec2_key.key_name"
     instance_type = "t2.micro"
     tags = {
         Name = "webserver2"
